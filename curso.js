0c33751c0734bd6ec937cbebdfc360b425d1f761
coldolmen <coldolmen@gmail.com>
2021-03-15 17:00:28 +0100

    mi primer video

---
+++ curso.js
@@ -0,0 +1,22 @@
+
+// Comment: logging something on the console!
+let name = 'Juan';
+let surName = 'Acosta';
+let age = 30;
+let person = {
+    name: 'Mosh',
+    age : 30
+}
+console.log(person.name);
+
+person['name'] = 'Mary';
+let selection = 'name';
+person[selection]
+
+let message ='My name is '+ name +' '+ surName;
+console.log(message);
+
+
+let selectedColors = ['red', 'blue'];
+selectedColors[2] = 'yellow';
+console.log(selectedColors.length);
